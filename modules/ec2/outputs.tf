d6ea42fbc50be07d4097406cc706d0229655ee1e
Zeynep <zeynepakbalikk@gmail.com>
2024-02-19 02:17:41 +0300

    refactor: clean code

--- modules/ec2/outputs.tf
+++ modules/ec2/outputs.tf
@@ -1,10 +1,3 @@
-# output "public_id" {
-#   value = try(aws_instance.ec2_instance, "")
-# }
-#
-# output "auto_scaling_group_id" {
-#   value = aws_autoscaling_group.demo-autoscale.id
-# }
 
 data "aws_autoscaling_groups" "demo_autoscale" {
   names = ["unique-demo3-autoscaling-group"]  # Specify the name of your Auto Scaling Group
