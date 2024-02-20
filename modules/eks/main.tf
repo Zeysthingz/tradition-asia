ca6078b81f7e762152cadfb68d5232c5bfbd6f05
Zeynep <zeynepakbalikk@gmail.com>
2024-02-20 10:04:59 +0300

    feat: add ami_type

--- modules/eks/main.tf
+++ modules/eks/main.tf
@@ -35,35 +35,7 @@ resource "local_file" "kubeconfig" {
   filename = "kubeconfig_${local.cluster_name}"
 }
 
-resource "aws_launch_template" "bottlerocket" {
-  name          = "bottlerocket-launch-template"
-  image_id      = "ami-034b92b7fa8598c1b"
-  instance_type = "t3.micro"
 
-  block_device_mappings {
-    device_name = "/dev/xvda"
-
-    ebs {
-      volume_size = 50 # Or your preferred volume size
-      volume_type = "gp3"
-    }
-  }
-
-  network_interfaces {
-    associate_public_ip_address = true
-  }
-
-  user_data = base64encode(<<-EOT
-[settings.kubernetes]
-api-server = ${data.aws_eks_cluster.cluster.endpoint}
-cluster-certificate = base64decode(${data.aws_eks_cluster.cluster.certificate_authority.0.data})
-cluster-name = "${local.cluster_name}"
-EOT
-  )
-}
-
-
-#https://registry.terraform.io/modules/terraform-aws-modules/eks/aws/latest/submodules/eks-managed-node-group
 module "eks" {
   source          = "terraform-aws-modules/eks/aws"
   version         = "18.30.3"
@@ -75,25 +47,13 @@ module "eks" {
 
   eks_managed_node_groups = {
     eks_node_group = {
-      desired_size    = 2
-      max_size        = 2
-      min_size        = 1
-      depends_on      = [aws_launch_template.bottlerocket]
-      launch_template = aws_launch_template.bottlerocket.id
+      ami_type       = "BOTTLEROCKET_x86_64"
+      desired_size   = 2
+      max_size       = 2
+      min_size       = 1
       instance_types = ["t2.micro"]
-    },
-    eks_node_group-v2 = {
-      desired_size           = 2
-      max_size               = 2
-      min_size               = 1
-      depends_on             = [aws_launch_template.bottlerocket]
-      launch_template_id     = aws_launch_template.bottlerocket.id
-      create_launch_template = false
-      instance_types         = ["t2.micro"]
-    },
-
+    }
   }
-
 }
 
 
